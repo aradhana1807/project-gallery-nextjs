16fd78c354e453376cfe315e9c09d89eb675af8e
Aradhana Nayak <aradhana.zero@gmail.com>
2024-06-04 11:30:41 +0530

    made visual changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { Desktop, GithubLogo, LinkedinLogo } from "@phosphor-icons/react/dist/ssr";
+import { Code , GithubLogo, LinkedinLogo } from "@phosphor-icons/react/dist/ssr";
 
 
 export default function Header() {
@@ -16,12 +16,12 @@ export default function Header() {
         </Link>
 
         <Link href='/' className='lg:hidden flex items-center gap-2'>
-          <Desktop size={32} /> Gallery
+          <Code  size={32} /> Gallery
         </Link>
 
         <div className='flex items-center gap-4'>
           <Link href='https://github.com/aradhana1807' >
-            <GithubLogo className='cursor-pointer transform hover:scale-125 transition duration-300 hover:text-neutral-500' size={32} />
+            <GithubLogo className='cursor-pointer transform hover:scale-125 transition duration-300 border-2 border-gray-500 p-[1px] rounded-full hover:text-black hover:border-black' size={32} weight="duotone"/>
           </Link>
 
           <Link href='https://www.linkedin.com/in/aradhana-nayak-607959212' >
